6c88ba0746a8e976d5df619e7c43537be6c97dee
HEROgold <martijnwieringa28@gmail.com>
2024-09-07 19:59:06 +0200

    add refpointer

--- abc_/pointers.py
+++ abc_/pointers.py
@@ -39,3 +39,16 @@ class TablePointer(ABC):
     @abstractmethod
     def from_table(cls, address: int, index: int) -> Self:
         pass
+
+class ReferencePointer(TablePointer):
+    def __init__(self, address: int, index: int, size: int) -> None:
+        log = logging.getLogger(f"{iris.name}.ReferencePointer.{self.__class__.__name__}")
+        log.debug(f"Creating {type(self).__name__} from {address=} + {index=} > {size=}")
+        self.address = address
+        self.index = index
+        self.pointer = address + index * size
+
+    @classmethod
+    @abstractmethod
+    def from_reference(cls, address: int, index: int, size: int) -> Self:
+        pass
