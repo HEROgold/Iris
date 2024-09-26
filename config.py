e4bf2bb1dbb9a5627de10cc024cac8cd92d4e66f
HEROgold <martijnwieringa28@gmail.com>
2024-09-26 17:33:44 +0200

    remove unused code/cleanup

--- config.py
+++ config.py
@@ -1,21 +1,11 @@
-from pathlib import Path
-from typing import Literal
-
 from enums.patches import Patch
 
 
 PROJECT_NAME = "Iris"
 VERSION = "0.1"
-TEXT_VERSION = "Zero One"
 AUTHORS = [{"name":"HEROgold", "email":""}]
 DESCRIPTION = "A Lufia II: Rise of the Sinistrals randomizer/patching toolkit."
 
-DEBUG_MODE = False
-PROFILE_MODE = True
-
-scale_custom_boss = None
-scale_custom_non_boss = None
-rom = Path(f"{__file__}").parent / "test_roms" / "Lufia II - Rise of the Sinistrals (USA).sfc"
 POINTER_SIZE = 2
 
 
@@ -50,19 +40,4 @@ ASCII_ART_COLORIZED = """
 \033[32m\\_______/\033[0m\033[33m|/   \\__/\033[0m\033[37m\\_______/\033[0m\033[31m\\_______)\033[0m
 """
 
-KNOWN_ROMS = {
-    "LUFIA2_NA": {"md5": "6efc477d6203ed2b3b9133c1cd9e9c5d", "filename": "tables_list.txt"},
-    "LUFIA2_JP": {"md5": "18567d21df6a95a7b52029d250bac721", "filename": "tables_list_jp.txt"},
-    "LUFIA2_FIX": {"md5": "026b649ed316448e038349e39a6fe579", "filename": "tables_list.fixxxer.txt"},
-    "LUFIA2_FRUE_V6": {"md5": "b58c76f2ac0b2aeb9b779e880d2bff18", "filename": "tables_list.frue.txt"},
-    "LUFIA2_SPEKKIO_V6": {"md5": "b4c4973a5f1cfd40bb1863dbad260de4", "filename": "tables_list.spekkio.txt"},
-    "LUFIA2_KUREJI_V6": {"md5": "5983201c45de814dc3672ab7d8279886", "filename": "tables_list.kureji.txt"},
-    "!LUFIA2_NA->LUFIA2_FIX": {"md5": "", "filename": "lufia2_fixxxer_unheadered.ips"},
-}
-
-BYTE_LENGTH = 8
-SHIFT_BYTE = 8
-MAX_2_BYTE = 0xFFFF
-BYTE_ORDER: Literal["big", "little"] = "little"
-PROJECT_PATH = Path(__file__).parent
 SUGGESTED_PATCH = Patch.FRUE
